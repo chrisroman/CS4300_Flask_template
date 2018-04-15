f08b4a3a592906565f7f80cc51463357d30d19a8
Mushaffar Khan <mushaffarkhan@dhcp-rhodes-1870.eduroam.cornell.edu>
2018-04-15 18:47:57 -0400

    Finihsed Thesaurus generation and jaccard on query

---
+++ expand_query.py
@@ -0,0 +1,38 @@
+from nltk.corpus import wordnet
+
+'''
+
+gets the synonyms associated with the input word and returns it as a list
+
+'''
+def synonyms(word):
+    synonyms = [] 
+    for syn in wordnet.synsets(word):
+        for l in syn.lemmas():
+            synonyms.append(l.name())
+    return synonyms
+
+'''
+Expands the given query using thesarus generation and appends the 
+synonyms to the end of the original query
+''' 
+
+def expand_query(query):
+    original_q = query
+    # dictionary = PyDictionary() 
+    query = original_q.split() 
+    synon = []
+    for w in query:
+        synon += synonyms(w)
+    n_query = [original_q]
+
+    if synon:
+
+        for words in synon: 
+            n_query.append(words.lower().replace("_"," "))
+
+        return n_query
+
+    else: 
+        return [original_q]
+
